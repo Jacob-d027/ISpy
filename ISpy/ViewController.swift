745064f61e4f83fa38cc6887953635fce249a265
Jacob Davis <jacobdofficial@gmail.com>
2023-10-13 23:30:16 -0600

    Initial Commit

---
+++ ISpy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Jacob Davis on 10/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
